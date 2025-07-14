0e6e44c35dbec1e871c6d7bc470979f2f68c6900
SEN9945 <139940616+SEN9945@users.noreply.github.com>
2025-07-14 16:27:48 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,11 @@
+
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.designmo.my.id/', // Ganti dengan URL utama Anda
+      url: 'https://www.designmo.my.id', // GANTI DENGAN URL UTAMA ANDA
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-    {
-      url: 'https://www.designmo.my.id/dashboard', // Ganti dengan URL halaman lain
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.designmo.my.id/dashboard/users', // Ganti dengan URL halaman lain
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.5,
     },
   ]
 }
